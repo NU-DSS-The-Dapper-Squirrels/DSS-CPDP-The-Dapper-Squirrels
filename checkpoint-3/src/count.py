fad26181cffd3900bec0c907bcdd7b41d3767c79
Xingbang Liu <xingbangliu@dhcp-10-105-230-200.wireless.northwestern.private>
2021-11-08 23:46:50 -0600

    data processing

---
+++ checkpoint-3/src/count.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import pprint as pp
+import json
+
+df = pd.read_csv(r"../222.csv", dtype={0: str, 1: float, 2: str})
+df = df[["date", "beat_id", "percent"]]
+# js = df.to_json(orient="records")
+# print(js)
+
+
+def retro_dictify(frame):
+    d = {}
+    for row in frame.values:
+        here = d
+        for elem in row[:-2]:
+            if elem not in here:
+                here[elem] = {}
+            here = here[elem]
+        here[row[-2]] = row[-1]
+    return d
+
+
+j = retro_dictify(df)
+pp.pprint(j)
+
+with open("out.json", "w") as f:
+    json.dump(j, f)
